29f0e6415949f93f238bbcffb995012a2df80340
Romnoel Petracorta <petracorta.rmnl@gmail.com>
2024-09-02 09:09:05 +0800

    Finished the logic

--- script.js
+++ script.js
@@ -1,20 +1,64 @@
 let computerScore = 0;
 let humanScore = 0;
+let computerChoice = "";
+let humanChoice = "";
 
 let getComputerChoice = () => {
   let choices = ["rock", "paper", "scissors"];
-  let computerChoice = choices[Math.floor(Math.random() * 3)];
+  computerChoice = choices[Math.floor(Math.random() * 3)];
   console.log(computerChoice);
 
   return computerChoice;
 };
 
-const rockButton = document.querySelector(".rock")
+let getHumanChoice = (choice) => {
+  return choice;
+};
 
-const paperButton = document.querySelector(".paper");
+const buttons = document.querySelectorAll("button");
 
-const scissorButton = document.querySelector(".scissor");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    humanChoice = getHumanChoice(button.className);
+    playRound(humanChoice, getComputerChoice());
+    checkGameStatus();
+  });
+});
 
-function getHumanChoice(event) {
-  
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Draw");
+  } else if (humanChoice == "rock" && computerChoice == "paper") {
+    computerScore++;
+    console.log("Comp own");
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    computerScore++;
+    console.log("Comp own");
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    humanScore++;
+    console.log("Human own");
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    humanScore++;
+    console.log("Human own");
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    humanScore++;
+    console.log("Human own");
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    computerScore++;
+    console.log("Comp own");
+  }
+
+  console.log(`Scores -> Human: ${humanScore}, Computer: ${computerScore}`);
+}
+
+function checkGameStatus() {
+  if (humanScore === 5) {
+    alert("Congratulations, you won!");
+    computerScore = 0;
+    humanScore = 0;
+  } else if (computerScore === 5) {
+    alert("AI will take over the world now.");
+    computerScore = 0;
+    humanScore = 0;
+  }
 }
